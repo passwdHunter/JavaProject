0657c5b0c92354692bfdfdbbdf2e12a53b59e56a
passwdHunter <shon.danem@mail.ru>
2025-05-03 23:39:40 +0300

    Добавил все контроллеры

---
+++ src/main/java/com/psu/cinema/controller/CinemaController.java
@@ -0,0 +1,9 @@
+package com.psu.cinema.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/cinema")
+public class CinemaController {
+}
